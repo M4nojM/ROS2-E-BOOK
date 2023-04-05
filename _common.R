12153de539dc661a87e8909a3343c5e806ece9cb
Rishikesavan R <techalla75@protonmail.com>
2023-04-05 19:13:35 +0530

    added preface

---
+++ _common.R
@@ -0,0 +1,28 @@
+status <- function(type) {
+  status <- switch(type,
+    polishing = "should be readable but is currently undergoing final polishing",
+    restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
+    drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
+    complete = "is largely complete and just needs final proof reading",
+    stop("Invalid `type`", call. = FALSE)
+  )
+
+  class <- switch(type,
+    polishing = "note",
+    restructuring = "important",
+    drafting = "important",
+    complete = "note"
+  )
+
+  cat(paste0(
+    "\n",
+    ":::: status\n",
+    "::: callout-", class, " \n",
+    "You are reading the work-in-progress edition of ROS 2 : Mea Via et Mea Disciplina. ",
+    "This chapter ", status, ". ",
+    "The complete version will be available here soon. ",
+    "Stay tuned!\n",
+    ":::\n",
+    "::::\n"
+  ))
+}
